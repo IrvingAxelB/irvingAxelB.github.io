7b736d7505fe639e33d613206ab1cadf1d18cdd2
Irving <Irvingb232@gmail.com>
2015-06-08 18:01:48 -0700

    Update
    Adding header, footer, and about me sections.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,24 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
+//
+//   //
+//
+// });
 
-  //
-
+$(function() {
+  smoothScrool(8000);
 });
+
+// smoothScrool function is applied from the document ready function
+function smoothScrool(duration) {
+  $('a[href^"#"]').on('click', function(event){
+
+    var target = $( $(this).attr('href') );
+
+    if(target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, duration);
+    }
+  });
+}
